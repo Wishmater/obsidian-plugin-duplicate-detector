1100b37f5ba76bb094c6be44c6455badd74e4182
Malek Bougadouha <bougadouha.malek@gmail.com>
2025-03-03 17:08:43 +0100

    Added ability to change highlight color

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,19 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
+import { App, PluginSettingTab, Setting } from "obsidian";
 import DuplicateDetectorPlugin, {
 	duplicateDetectorPluginSettings,
 } from "./main";
-
 
 // TODO 2 implement a way to refresh editor decorators when settings are changed, maybe with a ChangeNotifier pattern
 export interface DuplicateDetectorPluginSettings {
 	enableHighlighting: boolean;
 	enableNotices: boolean;
+	highlightColor: string;
 }
 
 export const DEFAULT_SETTINGS: Partial<DuplicateDetectorPluginSettings> = {
 	enableHighlighting: true,
 	enableNotices: true,
+	highlightColor: "#505000",
 };
 
 export class DuplicateDetectorSettingTab extends PluginSettingTab {
@@ -28,21 +29,24 @@ export class DuplicateDetectorSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Enable highlighting')
-			.setDesc('Enable highlighting of duplicate lines in editor')
-			.addToggle(component =>
+			.setName("Enable highlighting")
+			.setDesc("Enable highlighting of duplicate lines in editor")
+			.addToggle((component) =>
 				component
-					.setValue(duplicateDetectorPluginSettings.enableHighlighting)
+					.setValue(
+						duplicateDetectorPluginSettings.enableHighlighting
+					)
 					.onChange(async (value) => {
-						duplicateDetectorPluginSettings.enableHighlighting = value;
+						duplicateDetectorPluginSettings.enableHighlighting =
+							value;
 						await this.plugin.saveSettings();
 					})
 			);
 
 		new Setting(containerEl)
-			.setName('Enable notices')
-			.setDesc('Show a notice when pasting a duplicated line')
-			.addToggle(component =>
+			.setName("Enable notices")
+			.setDesc("Show a notice when pasting a duplicated line")
+			.addToggle((component) =>
 				component
 					.setValue(duplicateDetectorPluginSettings.enableNotices)
 					.onChange(async (value) => {
@@ -50,6 +54,17 @@ export class DuplicateDetectorSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					})
 			);
-	}
 
+		new Setting(containerEl)
+			.setName("Change Highlight Color")
+			.setDesc("Change the default highlight color")
+			.addColorPicker((component) =>
+				component
+					.setValue(duplicateDetectorPluginSettings.highlightColor)
+					.onChange(async (value) => {
+						duplicateDetectorPluginSettings.highlightColor = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
